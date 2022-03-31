daf0908291d249c58e316c567ccef794a5badfdc
Mauricio-MdS <mauricio.martins.silva@gmail.com>
2022-03-30 22:06:10 -0300

    Criacao do decorator inspect

--- app/dist/js/decorators/logar-tempo-de-execucao.js
+++ app/dist/js/decorators/logar-tempo-de-execucao.js
@@ -1,11 +1,17 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos = false) {
     return function (target, propertyKey, descriptor) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args) {
             const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`${propertyKey}, tempo de execução: ${t2 - t1}`);
+            let unidade = 'milisegundos';
+            let divisor = 1;
+            if (emSegundos) {
+                unidade = 'segundos';
+                divisor = 1000;
+            }
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / divisor} ${unidade}`);
             return retorno;
         };
         return descriptor;
--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao(){
+export function logarTempoDeExecucao(emSegundos: boolean = false){
     return function(
         target: any,
         propertyKey: string,
@@ -9,7 +9,13 @@ export function logarTempoDeExecucao(){
             const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance. now();
-            console.log(`${propertyKey}, tempo de execução: ${t2-t1}`);
+            let unidade = 'milisegundos';
+            let divisor = 1;
+            if (emSegundos){
+                unidade = 'segundos';
+                divisor = 1000;
+            }
+            console.log(`${propertyKey}, tempo de execução: ${(t2-t1)/divisor} ${unidade}`);
             return retorno;
         }
         return descriptor;
