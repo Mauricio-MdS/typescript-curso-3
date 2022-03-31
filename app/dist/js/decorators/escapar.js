4460e1c440ac3c371b0974f81c539fccad2a546c
Mauricio-MdS <mauricio.martins.silva@gmail.com>
2022-03-30 22:16:06 -0300

    Criacao do decorator escapar

---
+++ app/dist/js/decorators/escapar.js
@@ -0,0 +1,11 @@
+export function escapar(target, propertyKey, descriptor) {
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function (...args) {
+        let retorno = metodoOriginal.apply(this, args);
+        if (retorno.typeof === 'string') {
+            retorno.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        return retorno;
+    };
+    return descriptor;
+}
